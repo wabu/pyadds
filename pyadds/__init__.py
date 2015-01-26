d8cc845aec179175e9381827820ec25ffb512dcf
wabu <wabu@fooserv.net>
2015-01-26 11:27:47 +0100

    fixed new model issues

--- pyadds/__init__.py
+++ pyadds/__init__.py
@@ -1,6 +1,12 @@
-class AnythingType:
+class AnythingType(set):
     def __contains__(self, other):
         return True
+
+    def intersection(self, other):
+        return other
+
+    def union(self, other):
+        return self
 
     def __str__(self):
         return '*'
